db08f55de4063072c04c9702dcd314a885c21251
TimHsu <maochindada@gmail.com>
2020-10-25 01:34:46 +0800

    temp

--- System/init_ubuntu.sh
+++ System/init_ubuntu.sh
@@ -10,8 +10,8 @@ apt install bridge-utils -y
 apt install nmap -y
 apt install ntp -y
 
-apt install mariadb-server -y
-systemctl disable mysql
+# apt install mariadb-server -y
+# systemctl disable mysql
 # curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 # apt install git-lfs
 # git lfs install
