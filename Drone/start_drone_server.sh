1453141a3751a5d92aff285b7fcf2aaee3b55320
TimHsu@MBP <maochindada@gmail.com>
2020-12-29 23:35:53 +0800

    tmp

--- Drone/start_drone_server.sh
+++ Drone/start_drone_server.sh
@@ -8,7 +8,7 @@ docker run --name drone \
   --link gitea \
   --ip=172.20.10.99 \
   -v /var/lib/drone:/data \
-  -e DRONE_GITEA_SERVER=http://172.20.10.50/ \
+  -e DRONE_GITEA_SERVER=http://gitea/ \
   -e DRONE_GITEA_CLIENT_ID=614223ea-564a-4ee8-b174-eb456c84453e \
   -e DRONE_GITEA_CLIENT_SECRET=iyQnme5ksGju5zxJhdDaYAXvmywt6pmpqcts9VjKVeE=\
   -e DRONE_RPC_SECRET=f6b2b9931bdcc2dee834cbaf3d8aced2 \
