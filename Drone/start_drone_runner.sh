c6280c73a5de7acc02e725e7b53f8a4fd207c900
TimHsu@MBP <maochindada@gmail.com>
2020-12-29 22:47:20 +0800

    tmp

--- Drone/start_drone_runner.sh
+++ Drone/start_drone_runner.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-docker run -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -e DRONE_RPC_PROTO=https \
-  -e DRONE_RPC_HOST=drone.company.com \
-  -e DRONE_RPC_SECRET=super-duper-secret \
-  -e DRONE_RUNNER_CAPACITY=2 \
-  -e DRONE_RUNNER_NAME=${HOSTNAME} \
-  -p 3000:3000 \
+docker volume create drone-runner-vol
+docker run -d --name runner \
+  --network tocvlan \
+  --link drone \
+  --ip=172.20.10.98 \
   --restart always \
-  --name runner \
+  -v drone-runner-vol:/var/run/docker.sock \
+  -e DRONE_RPC_PROTO=http \
+  -e DRONE_RPC_HOST=drone \
+  -e DRONE_RPC_SECRET=f6b2b9931bdcc2dee834cbaf3d8aced2 \
+  -e DRONE_RUNNER_CAPACITY=2 \
+  -e DRONE_RUNNER_NAME=drone.runner.01 \
   drone/drone-runner-docker:1
