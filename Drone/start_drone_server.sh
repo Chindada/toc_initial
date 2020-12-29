90b7958905ffcdb26112b588996badde91c820c7
TimHsu@MBP <maochindada@gmail.com>
2020-12-29 10:54:10 +0800

    tmp

--- Drone/start_drone_server.sh
+++ Drone/start_drone_server.sh
@@ -3,14 +3,15 @@
 docker volume create drone-vol
 docker run --name=drone \
   --network=tocvlan \
+  --link=gitea \
   --ip=172.20.10.99 \
-  --volume=drone-vol:/data \
-  --env-file=./drone.env \
-  --env=DRONE_GITEA_SERVER=${DRONE_GITEA_SERVER} \
-  --env=DRONE_GITEA_CLIENT_ID=${DRONE_GITEA_CLIENT_ID} \
-  --env=DRONE_GITEA_CLIENT_SECRET=${DRONE_GITEA_CLIENT_SECRET} \
-  --env=DRONE_RPC_SECRET=${DRONE_RPC_SECRET} \
-  --env=DRONE_SERVER_HOST=${DRONE_SERVER_HOST} \
-  --env=DRONE_SERVER_PROTO=${DRONE_SERVER_PROTO} \
+  --env-file ./drone.env \
+  -v drone-vol:/data \
+  -e DRONE_GITEA_SERVER=${DRONE_GITEA_SERVER} \
+  -e DRONE_GITEA_CLIENT_ID=${DRONE_GITEA_CLIENT_ID} \
+  -e DRONE_GITEA_CLIENT_SECRET=${DRONE_GITEA_CLIENT_SECRET} \
+  -e DRONE_RPC_SECRET=${DRONE_RPC_SECRET} \
+  -e DRONE_SERVER_HOST=${DRONE_SERVER_HOST} \
+  -e DRONE_SERVER_PROTO=${DRONE_SERVER_PROTO} \
   --detach=true \
   drone/drone:1
