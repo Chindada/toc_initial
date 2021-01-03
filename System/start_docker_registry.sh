19705dff00ed4fd53357479181a20c31bf9eb9d2
TimHsu@MBP <maochindada@gmail.com>
2021-01-03 23:48:08 +0800

    tmp

--- System/start_docker_registry.sh
+++ System/start_docker_registry.sh
@@ -8,4 +8,5 @@ docker run -d \
   --ip=172.20.10.70 \
   --restart always \
   --name registry \
+  -e /home/toc/drone/toc_initial/System/credentials.yml:/etc/docker/registry/config.yml \
   registry:2
