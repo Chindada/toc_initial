969b0ce4ba368ae2044c2387077fde627bc060c5
TimHsu@MBP <maochindada@gmail.com>
2021-01-03 22:16:55 +0800

    tmp

--- System/start_registry_ui.sh
+++ System/start_registry_ui.sh
@@ -9,7 +9,6 @@ docker run -d \
   --restart always \
   --name registry_ui \
   --link registry \
-  -e REGISTRY_URL=http://registry:5000 \
   -e DELETE_IMAGES=true \
   -e REGISTRY_TITLE="ToC Registry" \
   joxit/docker-registry-ui:latest
