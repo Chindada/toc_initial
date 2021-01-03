a63a227f2070629f9a4d3c17b79b118c74bda20c
TimHsu@MBP <maochindada@gmail.com>
2021-01-03 22:08:47 +0800

    registry_ui

---
+++ System/start_registry_ui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker stop registry_ui
+docker system prune --volumes -f
+
+docker run -d \
+  --network tocvlan \
+  --ip=172.20.10.71 \
+  --restart always \
+  --name registry_ui \
+  --link registry \
+  -e REGISTRY_URL="http://registry:5000" \
+  -e DELETE_IMAGES=true \
+  -e REGISTRY_TITLE="ToC Registry" \
+  joxit/docker-registry-ui:latest
