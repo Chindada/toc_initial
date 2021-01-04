2b5b542e2c3d626af877794baa544260b5c4aac0
TimHsu@MBP <maochindada@gmail.com>
2021-01-04 23:33:25 +0800

    1

--- System/start_docker_registry.sh
+++ System/start_docker_registry.sh
@@ -8,5 +8,6 @@ docker run -d \
   --ip=172.20.10.70 \
   --restart always \
   --name registry \
+  -v /registry-data:/var/lib/registry \
   -e /home/toc/drone/toc_initial/System/credentials.yml:/etc/docker/registry/config.yml \
   registry:2
--- System/start_registry_ui.sh
+++ System/start_registry_ui.sh
@@ -8,6 +8,8 @@ docker run -d \
   --ip=172.20.10.71 \
   --restart always \
   --name registry-ui \
+  --link registry \
   -e DELETE_IMAGES=true \
-  -e REGISTRY_URL="http://172.20.10.70:5000" \
+  -e REGISTRY_URL=http://registry:5000 \
+  -e REGISTRY_TITLE=ToC Docker Registry \
   joxit/docker-registry-ui:static
