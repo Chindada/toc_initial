45fd1018f07668eb56e9726dbf97e9186e187026
TimHsu@MBP <maochindada@gmail.com>
2021-01-05 23:36:27 +0800

    red

--- Redmine/start_redmine.sh
+++ Redmine/start_redmine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker stop redmine
+docker system prune --volumes -f
+
 docker run -d --name redmine \
     --network=tocvlan \
     --ip=172.20.10.210 \
@@ -17,4 +20,4 @@ docker run -d --name redmine \
     -e SMTP_PORT=587 \
     -e SMTP_USER=minnotec.aiom@gmail.com \
     -e SMTP_PASSWORD=Mit82589155 \
-    redmine_aiom 
+    toc-redmine
