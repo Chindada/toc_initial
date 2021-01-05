bc63f4db96b5e461ce98e5806e0988af59bfe39d
TimHsu@MBP <maochindada@gmail.com>
2021-01-05 23:22:38 +0800

    test

---
+++ Redmine/start_redmine.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker run -d --name redmine \
+    --network=tocvlan \
+    --ip=172.20.10.210 \
+    --restart always \
+    --link tocsql \
+    -v redmine-vol:/usr/src/redmine/files \
+	-e TZ="Asia/Taipei" \
+    -e REDMINE_DB_MYSQL=tocsql \
+    -e REDMINE_DB_USERNAME=root \
+    -e REDMINE_DB_PASSWORD=asdf0000 \
+    -e REDMINE_DB_NAME=redmine \
+    -e REDMINE_USERNAME=admin \
+    -e REDMINE_PASSWORD=admin \
+    -e SMTP_HOST=smtp.gmail.com \
+    -e SMTP_PORT=587 \
+    -e SMTP_USER=minnotec.aiom@gmail.com \
+    -e SMTP_PASSWORD=Mit82589155 \
+    redmine_aiom 
--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -7,5 +7,5 @@ docker run --name nginx -d \
   --network tocvlan \
   --ip=172.20.10.220 \
   --restart always \
-  -v /home/toc/drone/toc_initial/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v ./nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx
