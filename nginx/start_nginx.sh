0a5e0b126a9a9929f96536c29d12aaeb2a66911e
TimHsu@MBP <maochindada@gmail.com>
2021-01-13 10:10:07 +0800

    3

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -8,5 +8,5 @@ docker run --name nginx -d \
   --ip=172.20.10.220 \
   --restart always \
   --link gitea \
-  -v $pwd/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v /home/toc/drone/toc_initial/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
   nginx
