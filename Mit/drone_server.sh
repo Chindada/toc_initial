68db5e7847c73388dae3dc746cbe3767180a992e
TimHsu@MBP <maochindada@gmail.com>
2021-01-25 11:31:04 +0800

    change id again

--- Mit/drone_server.sh
+++ Mit/drone_server.sh
@@ -2,18 +2,20 @@
 
 docker stop drone
 docker system prune --volumes -f
-
+rm -rf /docker_data/drone
 docker run --name drone \
   --network=dockervlan \
   --ip=192.168.10.236 \
   --restart always \
   -v /docker_data/drone:/data \
   -e DRONE_GITEA_SERVER=http://192.168.10.8/ \
-  -e DRONE_GITEA_CLIENT_ID=d805d516-a0ef-4f41-81e3-dce919fd36ba \
-  -e DRONE_GITEA_CLIENT_SECRET=-BkkDIy_jrCBQCd7Ad0ObIR7gvK18kr1N9wfF0s2DwzM= \
+  -e DRONE_GITEA_CLIENT_ID=8ec0c964-0166-47b4-a086-dd5cca059461 \
+  -e DRONE_GITEA_CLIENT_SECRET=QqesOgYpEKeSzfm4allwwb1bDbrXsOPM4QvhWftNfTo= \
   -e DRONE_RPC_SECRET=f6b2b9931bdcc2dee834cbaf3d8aced2 \
   -e DRONE_SERVER_HOST=192.168.10.236 \
   -e DRONE_SERVER_PROTO=http \
   -e DRONE_GIT_ALWAYS_AUTH=true \
   --detach=true \
   drone/drone:1
+
+
