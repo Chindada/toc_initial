173026a7380ca23b13e2aadea721ae3e670b386a
TimHsu@MBP <maochindada@gmail.com>
2021-02-02 20:19:49 +0800

    macOS

---
+++ macOS/mas_cli.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+brew install mas
+
+mas install 1295203466
+mas install 1153157709
+mas install 836500024
+mas install 985367838
+mas install 1444383602
+mas install 539883307
+mas install 462062816
+mas install 462054704
+mas install 462058435
