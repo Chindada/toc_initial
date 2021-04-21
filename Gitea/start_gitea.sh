89b8d7b3dc2c021618dbd41db0789ebcc8dce061
TimHsu@M1BP <maochindada@gmail.com>
2021-04-21 13:01:13 +0800

    gitea

--- Gitea/start_gitea.sh
+++ Gitea/start_gitea.sh
@@ -9,6 +9,10 @@ CREATE DATABASE gitea CHARACTER SET utf8 COLLATE utf8_general_ci;' >> tmp.sql
 mysql -u root -pasdf0000 -h 172.20.10.10 -P 3306 --force < tmp.sql
 rm tmp.sql
 
+groupadd -g 1500 git
+useradd -u 1500 -g 1500 -m git
+rm -rf /home/git/.ssh
+
 docker run --name gitea \
   --network=tocvlan \
   --ip=172.20.10.50 \
@@ -29,19 +33,9 @@ docker run --name gitea \
   -e DB_USER="root" \
   -e DB_PASSWD="asdf0000" \
   -e REQUIRE_SIGNIN_VIEW="true" \
-  -e USER_UID=0 \
+  -e USER_UID=1500 \
   -e USER_GID=0 \
   -v $HOME/gitea_data:/data \
-  -v $HOME/.ssh/:/data/git/.ssh \
+  -v /home/git/.ssh:/data/git/.ssh \
   -v /etc/localtime:/etc/localtime:ro \
   -d gitea/gitea:1.14.1
-
-# rm $HOME/.ssh/authorized_keys
-echo "$(cat $HOME/.ssh/id_ed25519.pub)" >> $HOME/.ssh/authorized_keys
-
-rm -rf /app
-mkdir /app
-mkdir /app/gitea
-echo 'ssh -o StrictHostKeyChecking=no git@172.20.10.50 "SSH_ORIGINAL_COMMAND=\"$SSH_ORIGINAL_COMMAND\" $0 $@"' >> /app/gitea/gitea
-chmod +x /app/gitea/gitea
-# chown -R git:git /app
