ae0d94aaddf6fa58bc3f1bdfa6a345a3d5561710
TimHsu@M1BP <maochindada@gmail.com>
2021-04-23 16:40:44 +0800

    tmp

--- Docker/post_docker.sh
+++ Docker/post_docker.sh
@@ -3,9 +3,14 @@
 groupadd docker
 usermod -aG docker $USER
 
-setting='{ "iptables": false, "bridge": "none" }'
+registries_ip="172.20.10.70"
+setting='
+{
+  "insecure-registries" : ["'$registries_ip':5000"],
+  "iptables": false, 
+  "bridge": "none"
+}'
 
 echo $setting > /etc/docker/daemon.json
 systemctl daemon-reload
 systemctl restart docker
-
